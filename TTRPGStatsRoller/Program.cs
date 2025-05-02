a33c2bc711a694b800a19424ff79ae5b0d69c9fe
Graham Arnett <GrahamKentArnett@outlook.com>
2025-05-01 23:22:04 -0400

    init commit

---
+++ TTRPGStatsRoller/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace TTRPGStatsRoller
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome traveler! This is my tabletop role-playing game stats roller!");
+
+            char redo;
+
+            string[] stats = { "STRENCH", "DEXTERITY", "CONSTITUTION", "INTELLIGENCE", "CHARISMA", "WISDOM" };
+            int[] scores = new int[6];
+
+            do
+            {
+                Console.Write("\n\n\nWould you like to go again? (y/n): ");
+                redo = Console.ReadKey().KeyChar;
+            } while (redo == 'y' || redo == 'Y');
+        }
+    }
+}
