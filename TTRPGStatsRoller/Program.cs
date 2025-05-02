d36a723657b62f33b93f4e8dbb104488773c30a5
Graham Arnett <GrahamKentArnett@outlook.com>
2025-05-02 00:06:05 -0400

    added the arrays I will be adding the scores to. Will fix the score/stat arrays first the others come later and require more extensive work

--- TTRPGStatsRoller/Program.cs
+++ TTRPGStatsRoller/Program.cs
@@ -7,12 +7,24 @@
             Console.WriteLine("Welcome traveler! This is my tabletop role-playing game stats roller!");
 
             char redo;
-
+            Random random = new Random();
             string[] stats = { "STRENCH", "DEXTERITY", "CONSTITUTION", "INTELLIGENCE", "CHARISMA", "WISDOM" };
             int[] scores = new int[6];
-
+            string[] classes = {"HIGHWAYMAN","BRIGAND","WILD ONE","DUELIST","PUGILIST","SURGEON","GAMBLER","PERFORMER","COWARD"};
+            string[] background = {"HERMIT","SOLDIER","SCHOLAR","MADMAN","APOTHECARY","ALCHEMIST","FOLK HERO","NAIVE IDEALIST","PRISONER",
+            "WASHED UP HERO", "FALLEN PATRICIAN","PARIAH","EXCOMMUNICATED FROM THE CLOTH", "HAUNTED"};
+            string[] race = {"ROOTLING","HUMAN","DWARF","ELF","HALFLING","ORC","BUGBEAR","GOBLIN","TIEFLING","KOBOLD"};
             do
             {
+                int classPick = random.Next(classes.Length);
+                int backgroundPick = random.Next(background.Length);
+                for (int i = 0; i < 6; i++)
+                {
+                    int scoreResult = random.Next(1, 21);
+                    scores[i] = scoreResult;
+                }
+                
+
                 Console.Write("\n\n\nWould you like to go again? (y/n): ");
                 redo = Console.ReadKey().KeyChar;
             } while (redo == 'y' || redo == 'Y');
